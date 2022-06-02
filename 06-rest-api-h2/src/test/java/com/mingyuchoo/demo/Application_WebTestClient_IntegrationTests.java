707777f05879643c23be806d03901eaec4fdbdbc
mingyuchoo <mingyuchoo@gmail.com>
2022-06-02 17:53:21 +0900

    added import statement

--- 06-rest-api-h2/src/test/java/com/mingyuchoo/demo/Application_WebTestClient_IntegrationTests.java
+++ 06-rest-api-h2/src/test/java/com/mingyuchoo/demo/Application_WebTestClient_IntegrationTests.java
@@ -1,6 +1,7 @@
 package com.mingyuchoo.demo;
 
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
--- 07-added-cucumber/src/test/java/com/mingyuchoo/demo/IntegrationTests_MockMvc_Application.java
+++ 07-added-cucumber/src/test/java/com/mingyuchoo/demo/IntegrationTests_MockMvc_Application.java
@@ -1,6 +1,7 @@
 package com.mingyuchoo.demo;
 
 import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
