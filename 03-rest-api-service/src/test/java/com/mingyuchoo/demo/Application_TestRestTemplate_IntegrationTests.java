a141e429e47dc78d5fee2cb76bb6f8ddd0772999
mingyuchoo <mingyuchoo@gmail.com>
2022-06-02 17:45:04 +0900

    import assertEquals

--- 03-rest-api-service/src/test/java/com/mingyuchoo/demo/Application_TestRestTemplate_IntegrationTests.java
+++ 03-rest-api-service/src/test/java/com/mingyuchoo/demo/Application_TestRestTemplate_IntegrationTests.java
@@ -1,6 +1,7 @@
 package com.mingyuchoo.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
