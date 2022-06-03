7f790b11ae5241cb8f0bb20157300f16aba3d2f2
mingyuchoo <mingyuchoo@gmail.com>
2022-06-04 07:10:40 +0900

    coffee-09-refactoring-extract-file

---
+++ coffee-service/coffee-09-refactoring-extract-file/src/main/java/com/example/coffee/CoffeeService.java
@@ -0,0 +1,37 @@
+package com.example.coffee;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CoffeeService {
+    //Examples:
+    //| 1. 브라질 산토스
+    //| 2. 콜롬비아 수프리모
+    //| 3. 자메이카 블루마운틴
+    //| 4. 에티오피아 예가체프
+    //| 5. 케냐 AA
+    //| 6. 코스타리카 따라주
+    //| 7. 탄자니아 AA (킬리만자로)
+    //| 8. 예멘 모카 마타리
+    //| 9. 하와이 코나
+    //| 10. 과테말라 안티구아
+    //| 11. 파나마 게이샤
+    //| 12. 엘살바도르
+
+
+
+    public List<CoffeeEntity> findAll() throws IOException {
+        List<CoffeeEntity> coffeeList;
+        File file = new File(this.getClass().getClassLoader().getResource("data-coffee.json").getFile());
+        ObjectMapper mapper = new ObjectMapper();
+        coffeeList = mapper.readValue(file, new TypeReference<List<CoffeeEntity>>() {});
+
+        return coffeeList;
+    }
+}
