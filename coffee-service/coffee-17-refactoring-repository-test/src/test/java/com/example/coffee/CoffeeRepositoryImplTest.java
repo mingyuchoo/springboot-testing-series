0b4f98b07edb4acdcfa43ac8aa2644287c87aa11
mingyuchoo <mingyuchoo@gmail.com>
2022-06-05 13:34:17 +0900

    did refactoring

--- coffee-service/coffee-17-refactoring-repository-test/src/test/java/com/example/coffee/CoffeeRepositoryImplTest.java
+++ coffee-service/coffee-17-refactoring-repository-test/src/test/java/com/example/coffee/CoffeeRepositoryImplTest.java
@@ -3,6 +3,9 @@ package com.example.coffee;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * Application -> Controller -> Service   -> Repository -> Entity
@@ -10,7 +13,11 @@ import org.junit.jupiter.api.Test;
  * (통합테스트)     (통합테스트)    (노노)        (노노)      -> (노노)
  */
 
+@ExtendWith(MockitoExtension.class)
 class CoffeeRepositoryImplTest {
+
+    @InjectMocks
+    private CoffeeRepository coffeeRepository = new CoffeeRepositoryImpl();
 
     @DisplayName("`data-coffee.json` 파일이 존재하는 상황에서, 해당 파일에서 값을 읽어 들였을 때, 데이터를 읽어 들여야 한다.")
     @Test
@@ -20,8 +27,7 @@ class CoffeeRepositoryImplTest {
         // When: 해당 파일에서 값을 읽어 들였을 때
         // Then: 데이터를 읽어 들여야 한다.
 
-        CoffeeRepositoryImpl coffeeRepositoryImpl = new CoffeeRepositoryImpl();
-        Assertions.assertNotNull(coffeeRepositoryImpl.findAll());
-        Assertions.assertEquals(12, coffeeRepositoryImpl.findAll().size());
+        Assertions.assertNotNull(coffeeRepository.findAll());
+        Assertions.assertEquals(12, coffeeRepository.findAll().size());
     }
 }
